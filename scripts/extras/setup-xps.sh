4085df7b850dd84444a89ae305351e7ad22261d7
Miljenko Šuflaj <headsouldev@gmail.com>
2023-01-27 01:54:35 +0100

    Add script for XPS 9570 tweaks

---
+++ scripts/extras/setup-xps.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+# Contains XPS 9570 tweaks, based on
+# https://github.com/JackHack96/dell-xps-9570-ubuntu-respin/blob/master/xps-tweaks.sh
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Must run as root"
+    exit 1
+fi
+
+# Update the repo
+apt update -y
+apt upgrade
+
+# Intel MicroCode
+apt install \
+    intel-microcode \
+    iucode-tool \
+-y
