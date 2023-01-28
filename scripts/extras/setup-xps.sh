060813fc11846601dff6163feb8e7cc30e62b76f
Miljenko Šuflaj <headsouldev@gmail.com>
2023-01-28 03:14:59 +0100

    Remove blob name from `setup-xps.sh`

--- scripts/extras/setup-xps.sh
+++ scripts/extras/setup-xps.sh
@@ -1,14 +1,14 @@
 #/bin/bash
 
 # Contains XPS 9570 tweaks, based on
-# https://github.com/JackHack96/dell-xps-9570-ubuntu-respin/blob/master/xps-tweaks.sh
+# https://github.com/JackHack96/dell-xps-9570-ubuntu-respin
 
 if [ "$EUID" -ne 0 ]; then
     echo "Must run as root"
     exit 1
 fi
 
-# Update the repo
+# Update & upgrade
 apt update -y
 apt upgrade -y
 
