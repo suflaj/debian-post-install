6c1c5a590149a2b3e63c357ef5851124b510a6e2
Miljenko Šuflaj <headsouldev@gmail.com>
2023-02-04 04:20:04 +0100

    Change user folders mapping

--- scripts/tweaks/tweak-home.sh
+++ scripts/tweaks/tweak-home.sh
@@ -15,20 +15,22 @@ NAMES=(
     "desktop"
     "documents"
     "downloads"
-    "music"
-    "pictures"
+    "documents/music"
+    "documents/pictures"
     "public"
-    "templates"
-    "videos"
+    "documents/templates"
+    "documents/videos"
 )
 
 for i in "${!COMPONENTS[@]}"; do
     component="${COMPONENTS[i]}"
     name="${NAMES[i]}"
 
-    new_path="\$HOME/${name}"
+    new_path="\${HOME}/${name}"
     var_name="XDG_${component}_DIR"
 
+    eval new_path_raw="${new_path}"
+    mkdir -p "${new_path_raw}"
     mv "$(xdg.user-dir ${component})" "${new_path}"
     sed -i "s|^${var_name}=.*$|${var_name}=\"${new_path}\"|" "${USER_DIRS_PATH}"
 done
