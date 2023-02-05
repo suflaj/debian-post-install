0aa00738bdaab9a6328e1ab3b903f8fbce163fb0
Miljenko Šuflaj <headsouldev@gmail.com>
2023-02-05 02:52:51 +0100

    Move prerequisites from init.sh to main README

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,42 +1,5 @@
 #!/bin/bash
 
-# This script assumes a minimal Debian install. Before this, you 
-# will need to connect to the internet. This can be done by 
-# following these steps:
-#
-# First find your adapter ID:
-#   sudo iwconfig
-# It's probably going to be named something like wlp3s0.
-#
-# Then you need to find the SSID of what you're connecting to:
-#   sudo iwlist ADAPTER_ID scan | grep "ESSID"
-# You can skip this step if you know the SSID already.
-#
-# Then, using the SSID and the password of the network, connect:
-#   nmcli d wifi connect SSID password PASSWORD
-# Congratulations, you're connected to the internet via WiFi.
-#
-#
-#
-# The last part is making sure that sudo is installed. If it 
-# isn't then do the following:
-#   su
-#   apt install sudo -y
-#
-# Then you'll need to add your user to sudoers. First, open the 
-# sudoers file:
-#   nano /etc/sudoers
-#
-# Then, add yourself under the line starting with root ALL=, ex.:
-#   suflaj ALL=(ALL:ALL) ALL
-#
-# Save the changes with CTRL+S, and exit with CTRL+X.
-#
-#
-#
-# After that, you should be able to use sudo, this script, and 
-# other ones.
-
 if [ "$EUID" -ne 0 ]; then
     echo "Must run as root"
     exit 1
